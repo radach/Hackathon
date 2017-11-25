60813fd84993bfe19fd6b6247cebe7b65217ac02
Luis Coimbra <lcoimbra2006@gmail.com>
2017-11-25 15:29:02 +0000

    simple notification

--- Mobile/app/src/main/java/com/example/carlos/gamify/HomeActivity.java
+++ Mobile/app/src/main/java/com/example/carlos/gamify/HomeActivity.java
@@ -1,6 +1,12 @@
 package com.example.carlos.gamify;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +28,7 @@ public class HomeActivity extends AppCompatActivity {
         final Button ver_breaktimes_favores_button = findViewById(R.id.ver_breaktimes_favores_button);
         final Button pedir_favor_button = findViewById(R.id.pedir_favor_button);
         final Button breaktime_button = findViewById(R.id.breaktime_button);
-
+        final Button button_testNotification = findViewById(R.id.button_notification);
 
         final Intent breaktime_intent = new Intent(this, BreaktimeActivity.class);
         final Intent perfil_intent = new Intent(this, PerfilActivity.class);
@@ -92,6 +98,40 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(perfil_intent);
             }
         });
+
+        button_testNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("vai notification");
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(view.getContext())
+                                .setSmallIcon(R.drawable.ic_launcher_background)
+                                .setContentTitle("My notification")
+                                .setContentText("Hello World!");
+
+                // Creates an explicit intent for an Activity in your app
+                Intent resultIntent = new Intent(view.getContext(), BreaktimeActivity.class);
+
+                // The stack builder object will contain an artificial back stack for the started Activity.
+                // This ensures that navigating backward from the Activity leads out of your application to the Home screen.
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(view.getContext());
+                // Adds the back stack for the Intent (but not the Intent itself)
+                stackBuilder.addParentStack(HomeActivity.class);
+                // Adds the Intent that starts the Activity to the top of the stack
+                stackBuilder.addNextIntent(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(
+                                0,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                mBuilder.setContentIntent(resultPendingIntent);
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                // mId allows you to update the notification later on.
+                Notification notif = mBuilder.build();
+                mNotificationManager.notify(33, notif);
+            }
+        });
+
     }
 
 
