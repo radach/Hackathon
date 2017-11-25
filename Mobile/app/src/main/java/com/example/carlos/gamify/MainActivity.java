2aa88c3a34ad2739589550c13cf47a667ac67653
Andre <andresilva2abril@gmail.com>
2017-11-25 04:15:59 +0000

    segunda tentativa

--- Mobile/app/src/main/java/com/example/carlos/gamify/MainActivity.java
+++ Mobile/app/src/main/java/com/example/carlos/gamify/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.carlos.gamify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.login_button);
+
+        final Intent intent = new Intent(this, HomeActivity.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                EditText email_input = (EditText) findViewById(R.id.email_input);
+                EditText password_input = (EditText) findViewById(R.id.password_input);
+                String email_value = email_input.getText().toString();
+                String password_value = password_input.getText().toString();
+
+                intent.putExtra("email", email_value);
+                intent.putExtra("password",password_value);
+
+                startActivity(intent);
+
+            }
+        });
     }
+
+
 }
